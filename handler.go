2864361ec333da679eb38cbb7de85541ce180ce3
Nathas Yingsukamol <new08242@gmail.com>
2019-09-24 03:35:28 +0700

    Wryyyy

--- handler.go
+++ handler.go
@@ -1,17 +1,37 @@
 package main
 
 import (
-	"net/http"
+	"encoding/json"
 	"fmt"
 	"io/ioutil"
+	"net/http"
 )
 
 func ReceiveMessageHandler(w http.ResponseWriter, r *http.Request) {
+	m := RawMessage{}
+
 	rawM, err := ioutil.ReadAll(r.Body)
 	if err != nil {
 		fmt.Errorf("read message from request error:", err)
 	}
 	defer r.Body.Close()
 
-	fmt.Println(fmt.Sprintf("message received: %+v", string(rawM)))
+	fmt.Println(fmt.Sprintf("raw message received: %+v \n", string(rawM)))
+
+	if err = json.Unmarshal(rawM, &m); err != nil {
+		fmt.Errorf("json unmarshal message error:", err)
+	}
+
+	//if err := json.NewDecoder(r.Body).Decode(&m); err != nil {
+	//	fmt.Errorf("json unmarshal message error: %s", err)
+	//}
+	//
+
+	fmt.Println(fmt.Sprintf("message received: %+v \n", m))
+
+	if m.Events[0].Message.Type == MessageTypeText {
+		if m.Events[0].Message.Text == "#hiew" {
+			fmt.Println("Wryyyy!!!! Zawarudo The World!!!!!")
+		}
+	}
 }
---
+++ message.go
@@ -0,0 +1,28 @@
+package main
+
+const (
+	MessageTypeText = "text"
+)
+
+type RawMessage struct {
+	Events []Event `json:"events"`
+}
+
+type Event struct {
+	Type       string  `json:"type"`
+	ReplyToken string  `json:"replyToken"`
+	Timestamp  int     `json:"timestamp"`
+	Source     Source  `json:"source"`
+	Message    Message `json:"message"`
+}
+
+type Source struct {
+	Type   string `json:"type"`
+	UserID string `json:"userId"`
+}
+
+type Message struct {
+	ID   string `json:"id"`
+	Type string `json:"type"`
+	Text string `json:"text"`
+}
